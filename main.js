c08ce76cbfbed9c9e045e6bea773eecaf618142f
blu2772 <tim@chipworker.de>
2024-02-15 22:13:41 +0100

    Neuer Order für assets und ein update des designs

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // Beispiel Daten
 const posts = [
-    { title: "Post 1", content: "Dies ist der Inhalt von Post 1" },
-    { title: "Post 2", content: "Dies ist der Inhalt von Post 2" },
-    { title: "Post 3", content: "Dies ist der Inhalt von Post 3" },
+    {id: 0, title: "Post 1", content: "Dies ist der Inhalt von Post 1",img:"sunset-beach-2.jpg" },
+    {id: 1, title: "Post 2", content: "Dies ist der Inhalt von Post 2",img:"" },
+    {id: 2, title: "Post 3", content: "Dies ist der Inhalt von Post 3",img:"" },
+    {id: 3, title: "Post 4", content: "Dies ist der Inhalt von Post 4",img:"" },
 
 ];
 
@@ -14,8 +15,9 @@ function generatePosts() {
 
         const postDiv = document.createElement('div');
         postDiv.classList.add('post');
+        postDiv.id = post.id;
         
-        const content = `<h2>${post.title}</h2><p>${post.content}</p>`;
+        const content = `<img src="src/${post.img}"><div class="content"><h2>${post.title}</h2><p>${post.content}</p></div>`;
         postDiv.innerHTML = content;
 
         feed.appendChild(postDiv);
