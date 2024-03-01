c63c31377700176d7d93e2154a6fa5c9ff99f051
blu2772 <tim@chipworker.de>
2024-03-01 22:14:10 +0100

    Edit & create post (Design anpassung)

--- Api/Connect.php
+++ Api/Connect.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+header('Access-Control-Allow-Methods: POST');
 
 $eingehendeDaten = file_get_contents('php://input');
 $daten = json_decode($eingehendeDaten, true);
@@ -49,7 +52,46 @@ switch ($daten['cmd']) {
              echo json_encode(['erfolg' => false, 'nachricht' => 'Fehler beim Abrufen der Daten']);
          }
         break;
+    case 'searchid':
+        $abfrage = "SELECT * FROM `feed` WHERE id = ".$daten['id'];
+        $ergebnis = $db->query($abfrage);
 
+        if ($ergebnis) {
+            $zeilen = $ergebnis->fetch_all(MYSQLI_ASSOC);
+            echo json_encode($zeilen);
+        } else {
+            echo json_encode(['erfolg' => false, 'nachricht' => 'Fehler beim Abrufen der Daten']);
+        }
+        break;
+    case 'edit':
+        $abfrage = "UPDATE `feed` SET `title` = '".$daten['title']."', `content` = '".$daten['content']."', `img` = '".$daten['img']."' WHERE id = ".$daten['id'];
+        $ergebnis = $db->query($abfrage);
+
+        if ($ergebnis) {
+            echo json_encode(['erfolg' => true, 'nachricht' => 'Daten erfolgreich bearbeitet']);
+        } else {
+            echo json_encode(['erfolg' => false, 'nachricht' => 'Fehler beim Bearbeiten der Daten']);
+        }
+        break;
+    case 'create':
+        $abfrage = "INSERT INTO `feed` (`title`, `content`, `img`) VALUES ('".$daten['title']."', '".$daten['content']."', '".$daten['img']."')";
+        $ergebnis = $db->query($abfrage);
+
+        if ($ergebnis) {
+            echo json_encode(['erfolg' => true, 'nachricht' => 'Daten erfolgreich erstellt']);
+        } else {
+            echo json_encode(['erfolg' => false, 'nachricht' => 'Fehler beim Erstellen der Daten']);
+        }
+        break;
+        case 'uploadImage':
+            $target_dir = "uploads/";
+            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                echo json_encode(['erfolg' => true, 'nachricht' => 'Bild erfolgreich hochgeladen', 'filePath' => $target_file]);
+            } else {
+                echo json_encode(['erfolg' => false, 'nachricht' => 'Fehler beim Hochladen des Bildes']);
+            }
+            break;
     default:
       
         echo json_encode(['erfolg' => false, 'nachricht' => 'Unbekannter Befehl']);
--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 // Beispiel Daten
-const posts = [
-    {id: 0, title: "Post 1", content: "Dies ist der Inhalt von Post 1",img:"sunset-beach-2.jpg" },
-    {id: 1, title: "Post 2", content: "Dies ist der Inhalt von Post 2",img:"" },
-    {id: 2, title: "Post 3", content: "Dies ist der Inhalt von Post 3",img:"" },
-    {id: 3, title: "Post 4", content: "Dies ist der Inhalt von Post 4",img:"" },
 
-];
 
 const url = "http://127.0.0.1/Facharbeit/Api/Connect.php";
 
@@ -53,7 +47,7 @@ async function generatePosts() {
 
 async function generatefeed(){
   const feed = document.querySelector('.feedcontainer');
-
+  feed.innerHTML = "";
   const readdata = {
       cmd: "read",
       limit: 4,
@@ -66,12 +60,143 @@ async function generatefeed(){
       postDiv.classList.add('post');
       postDiv.id = post.id;
       
-      const content = `<img class="img" src="src/${post.img}"><div class="content"><h2>${post.title}</h2></div><img class="delet" onClick="deletPost(${post.id})" src="src/recycle-bin.svg">`;
+      const content = `<img class="img" src="src/${post.img}">
+                        <div class="content">
+                         <h2>${post.title}</h2>
+                        </div>
+                        <img class="edit" onClick="openedit(${post.id})" src="src/pencil.svg">
+                        <img class="delet" onClick="deletPost(${post.id})" src="src/recycle-bin.svg">`;
       postDiv.innerHTML = content;
 
       feed.appendChild(postDiv);
   });
 
+}
+async function openedit(id){
+    const feed = document.querySelector('.apps');
+    const readdata = {
+        cmd: "searchid",
+        id: id,
+    };
+    const posts = await sendPostRequest(readdata);
+    console.log(posts);
+    const post = posts[0];
+    const postDiv = document.createElement('div');
+      
+      
+      const content = ` <div class="editor" >
+                            <div class="window">
+                                <h1>Edit</h1>
+                                <div class="in title">
+                                    <input type="text" id="title" name="title" value="${post.title}">
+                                </div>
+                                <div class="in content">
+                                    <textarea id="content" name="content" >${post.content}</textarea>
+                                </div>
+                                <div class="in img">
+                                    <input type="text" id="img" name="img" value="${post.img}">
+                                </div>
+                                <div class="submit">
+                                    <button onclick="submitedit()">Submit</button>
+                                </div>
+                            </div>
+                        </div>`;
+      postDiv.innerHTML = content;
+
+      feed.appendChild(postDiv);
+}
+async function uploadImage(file) {
+    const formData = new FormData();
+    formData.append('file', file);
+  
+    const response = await fetch(url, {
+      method: 'POST',
+      body: formData
+    });
+  
+    if (!response.ok) {
+      throw new Error('Netzwerkantwort war nicht ok.');
+    }
+  
+    const result = await response.json();
+    return result;
+  }
+async function newPost(){
+    const postDiv = document.createElement('div');
+      
+    const feed = document.querySelector('.apps');
+    const content = ` <div class="editor" >
+                          <div class="window">
+                              <h1>Edit</h1>
+                              <div class="in title">
+                                  <input type="text" id="title" name="title" placeholder="Title">
+                              </div>
+                              <div class="in content">
+                                  <textarea id="content" name="content" placeholder="Contnet"></textarea>
+                              </div>
+                              <div class="in img">
+                                  <input type="text" id="img" name="img" placeholder="img">
+                              </div>
+                              <div class="submit">
+                                  <button onclick="submitcreate()">Submit</button>
+                              </div>
+                          </div>
+                      </div>`;
+    postDiv.innerHTML = content;
+
+    feed.appendChild(postDiv);
+}
+async function uploadImage(file) {
+    const formData = new FormData();
+    formData.append('file', file);
+  
+    const response = await fetch(url, {
+      method: 'POST',
+      body: formData
+    });
+  
+    if (!response.ok) {
+      throw new Error('Netzwerkantwort war nicht ok.');
+    }
+  
+    const result = await response.json();
+    return result;
+  }
+async function submitcreate(){
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+    const img = document.getElementById('img').value;
+
+    const readdata = {
+        cmd: "create",
+        title: title,
+        content: content,
+        img: img,
+    };
+    const posts = await sendPostRequest(readdata);
+    closeEditor();
+    generatefeed();
+}
+
+function closeEditor(){
+    const feed = document.querySelector('.apps');
+    feed.innerHTML = "";
+}
+async function submitedit(){
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+    const img = document.getElementById('img').value;
+    const id = document.querySelector('.post').id;
+    const readdata = {
+        cmd: "edit",
+        id: id,
+        title: title,
+        content: content,
+        img: img,
+    };
+    const posts = await sendPostRequest(readdata);
+    closeEditor();
+    generatefeed();
 }
 async function deletPost(pID){
   const readdata = {
